a21721356a07bb9e6c4490b54fc84d52d9aabfef
ycnmhd <33285087+ycnmhd@users.noreply.github.com>
2024-08-24 09:53:06 +0100

    fix: empty item in file context menu (48)

--- src/obsidian/events/workspace/context-menu-itetms/add-toggle-view-menu-item.ts
+++ src/obsidian/events/workspace/context-menu-itetms/add-toggle-view-menu-item.ts
@@ -1,9 +1,10 @@
 import { Menu, TFile, WorkspaceLeaf } from 'obsidian';
 import Lineage from 'src/main';
-import { FILE_VIEW_TYPE } from 'src/view/view';
+import { LINEAGE_VIEW_TYPE } from 'src/view/view';
 import { lang } from 'src/lang/lang';
 import { customIcons } from 'src/helpers/load-custom-icons';
 import { toggleFileViewType } from 'src/obsidian/events/workspace/effects/toggle-file-view-type';
+import { getLeafOfFile } from 'src/obsidian/events/workspace/helpers/get-leaf-of-file';
 
 export const addToggleViewMenuItem = (
     menu: Menu,
@@ -11,10 +12,12 @@ export const addToggleViewMenuItem = (
     file: TFile,
     leaf: WorkspaceLeaf | undefined,
 ) => {
+    if (file.extension !== 'md') return;
     menu.addItem((item) => {
-        const view = leaf?.view;
-        if (!view) return;
-        const isTree = leaf.view.getViewType() === FILE_VIEW_TYPE;
+        if (!leaf) {
+            leaf = getLeafOfFile(plugin, file, 'lineage');
+        }
+        const isTree = leaf && leaf.view.getViewType() === LINEAGE_VIEW_TYPE;
         item.setTitle(isTree ? lang.open_in_editor : lang.open_in_lineage);
         item.setIcon(isTree ? 'file-text' : customIcons.cards.name);
 
