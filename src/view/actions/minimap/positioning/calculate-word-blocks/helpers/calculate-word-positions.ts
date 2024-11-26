497373c358097d069fdbe141bd4c3c24d3349695
ycnmhd <33285087+ycnmhd@users.noreply.github.com>
2024-11-26 16:56:10 +0100

    fix(minimap): new lines not rendered

--- src/view/actions/minimap/positioning/calculate-word-blocks/helpers/calculate-word-positions.ts
+++ src/view/actions/minimap/positioning/calculate-word-blocks/helpers/calculate-word-positions.ts
@@ -19,33 +19,38 @@ export const calculateWordPositions = (
 ): WordPositionResult => {
     let words: string[] = [];
     const isEmptyCard = !content.trim();
+    let lines: string[] = [];
     if (isEmptyCard) {
-        words.push('new');
+        lines.push('new');
     } else {
-        words = content.split(/\s+/);
+        lines = content.split('\n');
     }
     const positions: WordPosition[] = [];
     let currentLine = 0;
-    let currentX = 0;
+    for (const line of lines) {
+        let currentX = 0;
+        words = line.split(/\s+/);
 
-    for (let i = 0; i < words.length; i++) {
-        const word = words[i];
-        if (!word) continue;
-        const wordLength = word.length;
+        for (let i = 0; i < words.length; i++) {
+            const word = words[i];
+            if (!word) continue;
+            const wordLength = word.length;
 
-        if (currentX + wordLength > availableLineCharacters) {
-            currentLine++;
-            currentX = 0;
+            if (currentX + wordLength > availableLineCharacters) {
+                currentLine++;
+                currentX = 0;
+            }
+
+            positions.push({
+                word,
+                lineNumber: currentLine,
+                xInChars: currentX,
+                lengthInChars: wordLength,
+            });
+
+            currentX += wordLength + N_CHARS_OF_SPACE;
         }
-
-        positions.push({
-            word,
-            lineNumber: currentLine,
-            xInChars: currentX,
-            lengthInChars: wordLength,
-        });
-
-        currentX += wordLength + N_CHARS_OF_SPACE;
+        currentLine++;
     }
 
     return {
