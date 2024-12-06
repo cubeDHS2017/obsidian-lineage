f8e532d273eca06597db7deba651d8518d7fac6e
ycnmhd <33285087+ycnmhd@users.noreply.github.com>
2024-12-07 00:09:58 +0100

    fix(export): can't export from html-element format

--- src/obsidian/commands/helpers/export-document/prepare-exported-document.ts
+++ src/obsidian/commands/helpers/export-document/prepare-exported-document.ts
@@ -3,6 +3,7 @@ import { htmlCommentToJson } from 'src/lib/data-conversion/x-to-json/html-commen
 import { jsonToText } from 'src/lib/data-conversion/json-to-x/json-to-text';
 import { LineageDocumentFormat } from 'src/stores/settings/settings-type';
 import { outlineToJson } from 'src/lib/data-conversion/x-to-json/outline-to-json';
+import { htmlElementToJson } from 'src/lib/data-conversion/x-to-json/html-element-to-json';
 
 export const prepareExportedDocument = (
     fileData: string,
@@ -11,7 +12,11 @@ export const prepareExportedDocument = (
 ) => {
     const { data, frontmatter } = extractFrontmatter(fileData);
     const tree =
-        format === 'outline' ? outlineToJson(data) : htmlCommentToJson(data);
+        format === 'outline'
+            ? outlineToJson(data)
+            : format === 'html-element'
+              ? htmlElementToJson(data)
+              : htmlCommentToJson(data);
     if (tree.length < 2 && tree[0].children.length == 0) {
         throw new Error(`File ${basename} does not appear to be a tree`);
     }
