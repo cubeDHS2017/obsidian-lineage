c0d22dc2f8c49d1027d663c9e0f3774933021d55
ycnmhd <33285087+ycnmhd@users.noreply.github.com>
2024-12-13 00:48:19 +0100

    fix(search): ensure proper rotation when using nav buttons

--- src/view/components/container/toolbar/components/search/search-navigation-buttons.svelte
+++ src/view/components/container/toolbar/components/search/search-navigation-buttons.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { ChevronLeft, ChevronRight } from 'lucide-svelte';
+    import { ChevronDown, ChevronUp } from 'lucide-svelte';
     import Button from '../../../shared/button.svelte';
     import { searchStore } from 'src/stores/view/derived/search-store';
     import { getView } from 'src/view/components/container/context';
@@ -30,45 +30,44 @@
     );
 
     const selectNextResult = () => {
+        if (results.length === 0) return;
         const currentIndex = results.indexOf($activeNode);
-        const nextId = results[currentIndex + 1];
-        if (nextId) {
-            view.viewStore.dispatch({
-                type: 'DOCUMENT/SET_ACTIVE_NODE',
-                payload: { id: nextId },
-            });
-        }
+        const nextId = results[(currentIndex + 1) % results.length];
+        view.viewStore.dispatch({
+            type: 'DOCUMENT/SET_ACTIVE_NODE',
+            payload: { id: nextId },
+        });
     };
+
     const selectPreviousResult = () => {
-        const currentIndex = results.indexOf($activeNode);
-        const nextId = results[currentIndex - 1];
-        if (nextId) {
-            view.viewStore.dispatch({
-                type: 'DOCUMENT/SET_ACTIVE_NODE',
-                payload: { id: nextId },
-            });
-        }
+        if (results.length === 0) return;
+        const currentIndex = results.indexOf($activeNode) === -1 ? results.length : results.indexOf($activeNode);
+        const prevId = results[(currentIndex - 1 + results.length) % results.length];
+        view.viewStore.dispatch({
+            type: 'DOCUMENT/SET_ACTIVE_NODE',
+            payload: { id: prevId },
+        });
     };
 </script>
 
 <div class="search-container buttons-group">
     <Button
-        disabled={results.length===0||results.indexOf($activeNode) === 0}
+        disabled={results.length===0 }
         label={'Previous result'}
         on:click={selectPreviousResult}
         tooltipPosition="bottom"
     >
-        <ChevronLeft class="svg-icon" size="12" />
+        <ChevronUp class="svg-icon" size="12" />
     </Button>
 
 
     <Button
-        disabled={results.length===0||results.indexOf($activeNode) === results.length - 1}
+        disabled={results.length===0 }
         label={'Next result'}
         on:click={selectNextResult}
         tooltipPosition="bottom"
     >
-        <ChevronRight class="svg-icon" size="12" />
+        <ChevronDown class="svg-icon" size="12" />
     </Button>
     <div class="search-stats">
         {`${results.indexOf($activeNode) + 1} / ${results.length}`}
