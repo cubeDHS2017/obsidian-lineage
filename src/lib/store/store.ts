cubeDHS2017/obsidian-lineage

commit c414243a185bdc0ce4a22ea0d754997935550048

tweak: don't align branch when clicking on container

src/lib/store/store.ts
@@ -6,7 +6,7 @@ export type Subscriber<T, U> = (
     firstRun?: boolean,
 ) => void;
 
-export type Reducer<T, U> = (store: T, action: U) => T;
+export type Reducer<T, U> = (store: T, action: U) => T | null;
 
 export type OnError<U> = (
     error: Error,
@@ -69,8 +69,11 @@ export class Store<T, U> implements Writable<T> {
         while (this.actionQueue.length > 0) {
             const action = this.actionQueue.shift();
             try {
-                this.value = this.reducer(this.value, action!);
-                this.notifySubscribers(action);
+                const newValue = this.reducer(this.value, action!);
+                if (newValue) {
+                    this.value = newValue;
+                    this.notifySubscribers(action);
+                }
             } catch (error) {
                 this.onError(error, 'reducer', action);
             }

src/stores/document/document-reducer.ts
@@ -43,8 +43,8 @@ const updateDocumentState = (
     let affectedNodeId: null | string = null;
     let affectedNodeContent: Content[string] | null = null;
     if (action.type === 'DOCUMENT/SET_NODE_CONTENT') {
-        const success = setNodeContent(state.document.content, action);
-        if (!success) return;
+        const update = setNodeContent(state.document.content, action);
+        if (!update) return false;
         newActiveNodeId = action.payload.nodeId;
     } else if (action.type === 'DOCUMENT/INSERT_NODE') {
         newActiveNodeId = insertNode(state.document, action);
@@ -59,13 +59,13 @@ const updateDocumentState = (
         affectedNodeId = action.payload.activeNodeId;
     } else if (action.type === 'DOCUMENT/EXTRACT_BRANCH') {
         affectedNodeContent = state.document.content[action.payload.nodeId];
-        const success = setNodeContent(state.document.content, {
+        const update = setNodeContent(state.document.content, {
             payload: {
                 nodeId: action.payload.nodeId,
                 content: `[[${action.payload.documentName}]]`,
             },
         });
-        if (!success) return;
+        if (!update) return false;
         removeExtractedBranch(state.document, action);
         newActiveNodeId = action.payload.nodeId;
     } else if (action.type === 'DOCUMENT/SPLIT_NODE') {
@@ -195,12 +195,14 @@ const updateDocumentState = (
         addSnapshot(state.document, state.history, context);
         state.history = { ...state.history };
     }
+    return true;
 };
 
 export const documentReducer = (
     store: DocumentState,
     action: DocumentStoreAction,
-): DocumentState => {
-    updateDocumentState(store, action);
+) => {
+    const update = updateDocumentState(store, action);
+    if (!update) return null;
     return store;
 };

src/lang/lang.ts
@@ -251,6 +251,7 @@ export const lang = {
     modals_rules_no_rules: 'No rules',
     controls_rules: 'Rule-based styles',
     modals_rules_matches: 'Number of matches',
+    modals_rules_drag_handle: 'Change priority',
 
     // settings
     controls_settings: 'Settings',

src/main.ts
@@ -57,11 +57,11 @@ export default class Lineage extends Plugin {
             LINEAGE_VIEW_TYPE,
             (leaf) => new LineageView(leaf, this),
         );
+        addCommands(this);
+        loadCommands(this);
         this.registerPatches();
         this.registerEffects();
         this.registerEvents();
-        addCommands(this);
-        loadCommands(this);
         this.statusBar = new StatusBar(this);
         this.loadRibbonIcon();
         this.registerMarkdownPostProcessor(

src/stores/view/subscriptions/on-view-mount.ts
@@ -62,4 +62,5 @@ export const onViewMount = (view: LineageView) => {
     view.rulesProcessor.onRulesUpdate();
     view.zoomFactor = view.plugin.settings.getValue().view.zoomLevel;
     applyViewSize(view);
+    view.alignBranch.align({ type: 'view/life-cycle/mount' });
 };

src/stores/view/subscriptions/on-view-state-update.ts
@@ -72,9 +72,8 @@ export const onViewStateUpdate = (
 
     // effects
     if (activeNodeChange || e.search || e.editMainSplit) {
-        const skipAligning =
-            action.type === 'DOCUMENT/SET_ACTIVE_NODE' &&
-            action.context?.modKey;
+        // @ts-ignore
+        const skipAligning = 'context' in action && action.context?.modKey;
         if (!skipAligning) {
             view.alignBranch.align();
         }

src/stores/view/view-store-actions.ts
@@ -47,6 +47,9 @@ export type ToggleEditModeAction = {
 
 export type DisableEditModeAction = {
     type: 'view/main/disable-edit';
+    context?: {
+        modKey?: boolean;
+    };
 };
 
 export type ViewDocumentAction =
@@ -118,6 +121,9 @@ export type EnableEditInSidebar = {
 
 export type DisableEditInSidebar = {
     type: 'view/sidebar/disable-edit';
+    context?: {
+        modKey?: boolean;
+    };
 };
 
 export type StyleRulesViewActions = UpdateStyleRulesResultAction;

src/view/actions/keyboard-shortcuts/helpers/commands/commands/helpers/save-node-content.ts
@@ -1,6 +1,6 @@
 import { LineageView } from 'src/view/view';
 
-export const saveNodeContent = (view: LineageView) => {
+export const saveNodeContent = (view: LineageView, modKey = false) => {
     if (view.inlineEditor.nodeId) {
         view.inlineEditor.unloadNode();
         const isInSidebar =
@@ -8,10 +8,16 @@ export const saveNodeContent = (view: LineageView) => {
         if (isInSidebar) {
             view.viewStore.dispatch({
                 type: 'view/sidebar/disable-edit',
+                context: {
+                    modKey,
+                },
             });
         } else {
             view.viewStore.dispatch({
                 type: 'view/main/disable-edit',
+                context: {
+                    modKey,
+                },
             });
         }
     }

commit 19142727ceb15b06aa564ed651e960ed09492601

fix: regression introduced in c414243

src/lib/store/store.ts
@@ -1,12 +1,14 @@
 import { Unsubscriber, Updater, Writable } from 'svelte/store';
 
+export const NO_UPDATE = Symbol('NO_UPDATE');
+
 export type Subscriber<T, U> = (
     value: T,
     action?: U,
     firstRun?: boolean,
 ) => void;
 
-export type Reducer<T, U> = (store: T, action: U) => T | null;
+export type Reducer<T, U> = (store: T, action: U) => T | typeof NO_UPDATE;
 
 export type OnError<U> = (
     error: Error,
@@ -70,7 +72,7 @@ export class Store<T, U> implements Writable<T> {
             const action = this.actionQueue.shift();
             try {
                 const newValue = this.reducer(this.value, action!);
-                if (newValue) {
+                if (newValue !== NO_UPDATE) {
                     this.value = newValue;
                     this.notifySubscribers(action);
                 }

src/lib/tree-utils/get/traverse-down.ts
@@ -10,8 +10,8 @@ export const traverseDown = (
     const result: string[] = [];
     let nodeColumnIndex = 0;
     if (cleanDocument) {
-        nodeColumnIndex = findNodeColumn(columns, nodeId);
-        if (nodeColumnIndex >= columns.length - 1) {
+        nodeColumnIndex = findNodeColumn(columns, nodeId) + 1;
+        if (nodeColumnIndex > columns.length - 1) {
             return result;
         }
     }
@@ -20,7 +20,7 @@ export const traverseDown = (
 
     let firstResult = false;
 
-    for (let i = nodeColumnIndex + 1; i < columns.length; i++) {
+    for (let i = nodeColumnIndex; i < columns.length; i++) {
         const column = columns[i];
         let columnResult = false;
         for (const group of column.groups) {

src/stores/document/document-reducer.ts
@@ -34,6 +34,7 @@ import { removeStalePinnedNodes } from 'src/stores/document/reducers/pinned-node
 import { loadPinnedNodes } from 'src/stores/document/reducers/pinned-nodes/load-pinned-nodes';
 import { refreshGroupParentIds } from 'src/stores/document/reducers/meta/refresh-group-parent-ids';
 import { loadDocumentFromJSON } from 'src/stores/document/reducers/load-document-from-file/load-document-from-json';
+import { NO_UPDATE } from 'src/lib/store/store';
 
 const updateDocumentState = (
     state: DocumentState,
@@ -44,7 +45,7 @@ const updateDocumentState = (
     let affectedNodeContent: Content[string] | null = null;
     if (action.type === 'DOCUMENT/SET_NODE_CONTENT') {
         const update = setNodeContent(state.document.content, action);
-        if (!update) return false;
+        if (!update) return NO_UPDATE;
         newActiveNodeId = action.payload.nodeId;
     } else if (action.type === 'DOCUMENT/INSERT_NODE') {
         newActiveNodeId = insertNode(state.document, action);
@@ -65,7 +66,7 @@ const updateDocumentState = (
                 content: `[[${action.payload.documentName}]]`,
             },
         });
-        if (!update) return false;
+        if (!update) return NO_UPDATE;
         removeExtractedBranch(state.document, action);
         newActiveNodeId = action.payload.nodeId;
     } else if (action.type === 'DOCUMENT/SPLIT_NODE') {
@@ -195,14 +196,13 @@ const updateDocumentState = (
         addSnapshot(state.document, state.history, context);
         state.history = { ...state.history };
     }
-    return true;
 };
 
 export const documentReducer = (
     store: DocumentState,
     action: DocumentStoreAction,
 ) => {
-    const update = updateDocumentState(store, action);
-    if (!update) return null;
+    const result = updateDocumentState(store, action);
+    if (result === NO_UPDATE) return NO_UPDATE;
     return store;
 };

src/view/components/container/left-sidebar/components/pinned-cards/actions/scroll-active-pinned-node.ts
@@ -1,0 +1,40 @@
+import { ActivePinnedCardStore } from 'src/stores/view/derived/pinned-cards-sidebar';
+import { scrollCardIntoView } from '../../recent-cards/helpers/scroll-card-into-view';
+import { PinnedNodesStore } from 'src/stores/document/derived/pinned-nodes-store';
+import { getView } from 'src/view/components/container/context';
+
+export const scrollActivePinnedNode = (containerRef: HTMLElement) => {
+    const view = getView();
+    let timeout: ReturnType<typeof setTimeout> | null = null;
+
+    const activePinnedCardStore = ActivePinnedCardStore(view);
+    const activePinnedNodeSub = activePinnedCardStore.subscribe(
+        (activeNodeId) => {
+            if (timeout) clearTimeout(timeout);
+            timeout = setTimeout(() => {
+                if (!containerRef) return;
+                if (!activeNodeId) return;
+                scrollCardIntoView(containerRef, activeNodeId);
+            }, 200);
+        },
+    );
+
+    const pinnedNodesStore = PinnedNodesStore(view);
+    const pinnedNodesSub = pinnedNodesStore.subscribe(() => {
+        if (timeout) clearTimeout(timeout);
+        timeout = setTimeout(() => {
+            if (!containerRef) return;
+            const activeNodeId =
+                view.viewStore.getValue().pinnedNodes.activeNode;
+            if (!activeNodeId) return;
+            scrollCardIntoView(containerRef, activeNodeId);
+        }, 200);
+    });
+
+    return {
+        destroy: () => {
+            activePinnedNodeSub();
+            pinnedNodesSub();
+        },
+    };
+};

src/view/components/container/left-sidebar/components/pinned-cards/pinned-cards-sidebar.svelte
@@ -6,15 +6,13 @@
     import { documentStateStore } from '../../../../../../stores/view/derived/editing-store';
     import { IdSectionStore } from '../../../../../../stores/document/derived/id-section-store';
     import { ActivePinnedCardStore } from '../../../../../../stores/view/derived/pinned-cards-sidebar';
-    import {
-        scrollCardIntoView
-    } from 'src/view/components/container/left-sidebar/components/recent-cards/helpers/scroll-card-into-view';
-    import { onDestroy } from 'svelte';
     import NoItems from '../no-items/no-items.svelte';
     import { PendingConfirmationStore } from 'src/stores/view/derived/pending-confirmation';
     import { NodeStylesStore } from 'src/stores/view/derived/style-rules';
+    import {
+        scrollActivePinnedNode
+    } from 'src/view/components/container/left-sidebar/components/pinned-cards/actions/scroll-active-pinned-node';
 
-    let containerRef: HTMLElement | null = null;
     const view = getView();
     const pinnedNodesArray = PinnedNodesStore(view);
 
@@ -24,35 +22,11 @@
     const activePinnedCard = ActivePinnedCardStore(view);
     const pendingConfirmation = PendingConfirmationStore(view);
     const styleRules = NodeStylesStore(view);
-    const subscriptions: (() => void)[] = [];
-    subscriptions.push(
-        ActivePinnedCardStore(view).subscribe((activeNodeId) => {
-            setTimeout(() => {
-                if (!containerRef) return;
-                if (!activeNodeId) return;
-                scrollCardIntoView(containerRef, activeNodeId);
-            }, 200);
-        }),
-    );
-    subscriptions.push(
-        PinnedNodesStore(view).subscribe(() => {
-            setTimeout(() => {
-                if (!containerRef) return;
-                const activeNodeId =
-                    view.viewStore.getValue().pinnedNodes.activeNode;
-                if (!activeNodeId) return;
-                scrollCardIntoView(containerRef, activeNodeId);
-            }, 200);
-        }),
-    );
-    onDestroy(() => {
-        for (const unsub of subscriptions) {
-            unsub();
-        }
-    });
+
+
 </script>
 
-<div class="pinned-cards-container" bind:this={containerRef}>
+<div class="pinned-cards-container" use:scrollActivePinnedNode>
     {#if $pinnedNodesArray.length > 0}
         {#each $pinnedNodesArray as node (node)}
             <Node
@@ -62,9 +36,8 @@
                     : ActiveStatus.sibling}
                 editing={$editingStateStore.activeNodeId === node &&
                     $editingStateStore.isInSidebar === true}
-                confirmDisableEdit={$editingStateStore.activeNodeId ===
-                    node &&
-                    $pendingConfirmation.disableEdit===node &&
+                confirmDisableEdit={$editingStateStore.activeNodeId === node &&
+                    $pendingConfirmation.disableEdit === node &&
                     $editingStateStore.isInSidebar === true}
                 confirmDelete={$pendingConfirmation.deleteNode.has(node)}
                 isInSidebar={true}