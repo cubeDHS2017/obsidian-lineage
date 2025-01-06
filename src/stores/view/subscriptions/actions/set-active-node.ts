cubeDHS2017/obsidian-lineage

commit 31c13333f0e4d1b48c95df6de21e3c682ec6d907

enhance(snapshots): show affected section when selecting previous snapshot

src/stores/view/subscriptions/actions/set-active-node.ts
@@ -1,29 +1,46 @@
 import { LineageView } from 'src/view/view';
 import { getIdOfSection } from 'src/stores/view/subscriptions/helpers/get-id-of-section';
 import { DocumentStoreAction } from 'src/stores/document/document-store-actions';
+import { Snapshot } from 'src/stores/document/document-state-type';
 
 export const setActiveNode = (
     view: LineageView,
     action: DocumentStoreAction,
 ) => {
     const documentState = view.documentStore.getValue();
-    let shouldSetActiveNode =
-        (view.isViewOfFile && action.type !== 'DOCUMENT/DROP_NODE') ||
-        action.type === 'DOCUMENT/LOAD_FILE';
-    if (!shouldSetActiveNode) {
-        const activeNodeOfView = view.viewStore.getValue().document.activeNode;
-        const activeNodeDoesNotExist =
-            !documentState.sections.id_section[activeNodeOfView];
-        shouldSetActiveNode = activeNodeDoesNotExist;
+    const viewState = view.viewStore.getValue();
+
+    const activeNodeOfView = viewState.document.activeNode;
+    const activeSectionOfView =
+        documentState.sections.id_section[activeNodeOfView];
+    const activeNodeExists = !!activeSectionOfView;
+
+    let newActiveSection = documentState.history.context.activeSection;
+    let shouldSetActiveNode = true;
+
+    if (activeNodeExists) {
+        // keep the affected active section when undoing
+        if (action.type === 'HISTORY/APPLY_PREVIOUS_SNAPSHOT') {
+            const state = documentState.history.state;
+            const previousSnapshot: Snapshot =
+                documentState.history.items[state.activeIndex + 1];
+            newActiveSection = previousSnapshot.context.affectedSection;
+        }
+        // active view of file should always update except for dnd events
+        else if (view.isViewOfFile && action.type === 'DOCUMENT/DROP_NODE') {
+            shouldSetActiveNode = false;
+        }
+        // unless the active node does not exist, don't update other views
+        else if (!view.isActive) {
+            shouldSetActiveNode = false;
+        }
     }
+
     if (shouldSetActiveNode) {
         view.viewStore.dispatch({
             type: 'DOCUMENT/SET_ACTIVE_NODE',
             payload: {
-                id: getIdOfSection(
-                    documentState.sections,
-                    documentState.history.context.activeSection,
-                ),
+                id: getIdOfSection(documentState.sections, newActiveSection),
             },
         });
     }

src/stores/view/subscriptions/on-view-mount.ts
@@ -25,7 +25,7 @@ const applySettingsToView = (view: LineageView) => {
     applyContainerBg(view, state.view.theme.containerBg);
     applyActiveBranchBg(view, state.view.theme.activeBranchBg);
     applyCardWidth(view, state.view.cardWidth);
-    applyCardIndentationWidth(view, state.view.cardWidth);
+    applyCardIndentationWidth(view, state.view.nodeIndentationWidth);
     applyColumnsGap(view, state.view.columnsGap);
     applyCardsGap(view, state.view.cardsGap);
     if (!view.container) return;

commit 4f082a96d1623410e5b7831aff3ff590e0b45590

fix: regression introduced in 31c1333

src/stores/view/subscriptions/actions/set-active-node.ts
@@ -11,8 +11,9 @@ export const setActiveNode = (
     const viewState = view.viewStore.getValue();
 
     const activeNodeOfView = viewState.document.activeNode;
-    const activeSectionOfView =
-        documentState.sections.id_section[activeNodeOfView];
+    const id_section = documentState.sections.id_section;
+    const section_id = documentState.sections.section_id;
+    const activeSectionOfView = id_section[activeNodeOfView];
     const activeNodeExists = !!activeSectionOfView;
 
     let newActiveSection = documentState.history.context.activeSection;
@@ -24,7 +25,10 @@ export const setActiveNode = (
             const state = documentState.history.state;
             const previousSnapshot: Snapshot =
                 documentState.history.items[state.activeIndex + 1];
-            newActiveSection = previousSnapshot.context.affectedSection;
+            const affectedSection = previousSnapshot.context.affectedSection;
+            if (section_id[affectedSection]) {
+                newActiveSection = affectedSection;
+            }
         }
         // active view of file should always update except for dnd events
         else if (view.isViewOfFile && action.type === 'DOCUMENT/DROP_NODE') {